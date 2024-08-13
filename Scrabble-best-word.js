16b9b2aadf0cef956ae07ba6f4118cc0007be729
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-13 10:55:42 -0700

    Update Scrabble-best-word.js

--- Scrabble-best-word.js
+++ Scrabble-best-word.js
@@ -12,3 +12,15 @@ If the length and the score are the same for two elements, return the index of t
 
 
 // Solution
+
+function getBestWord(points,words){
+	let by_score     = (a,b) => scoreWord(points,a) - scoreWord(points,b)
+  let by_shortest  = (a,b) => b.length - a.length 
+  let by_index     = (a,b) => words.indexOf(b) - words.indexOf(a)
+  let sorted = words.slice().sort( (a,b) => by_score(a,b) || by_shortest(a,b) || by_index(a,b) )
+  return words.indexOf(sorted.pop())
+}
+
+function scoreWord(points,word){
+	return word.split('').map( c => points[c.charCodeAt(0)-65] ).reduce( (a,b) => a+b )
+}
