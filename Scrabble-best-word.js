2b32f26a599af0f2b3fc45b8f2793f1cd616382f
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-13 10:54:33 -0700

    Create Scrabble-best-word.js

---
+++ Scrabble-best-word.js
@@ -0,0 +1,11 @@
+/*
+You're playing scrabble, but counting points is hard.
+You decide to create a script to calculate the best possible value.
+
+The function takes two arguments :
+ `points` : an array of integers representing each of the letters from A to Z and their points.
+  `words` : an array of strings, uppercase.
+
+You must return the index of the shortest word which realizes the highest score.
+If the length and the score are the same for two elements, return the index of the first one.
+*/
